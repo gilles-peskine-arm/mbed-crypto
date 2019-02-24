c37ccf8bfd490f56369070e78de33bc0446ba5d7
Gilles Peskine <Gilles.Peskine@arm.com>
2019-02-24 17:47:27 +0100

    Remove compilation option MBEDTLS_PSA_HAS_ITS_IO
    MBEDTLS_PSA_HAS_ITS_IO is not really useful since it doesn't actually
    enable anything except the entropy seed file support, which only
    requires the ITS interface and not a native implemetation. Remove it.

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -1146,16 +1146,6 @@
  */
 //#define MBEDTLS_ENTROPY_NV_SEED
 
-/**
- * \def MBEDTLS_PSA_HAS_ITS_IO
- *
- * Enable the non-volatile secure storage usage.
- *
- * This is crucial on systems that do not have a HW TRNG support.
- *
- */
-//#define MBEDTLS_PSA_HAS_ITS_IO
-
 /* MBEDTLS_PSA_CRYPTO_KEY_FILE_ID_ENCODES_OWNER
  *
  * In PSA key storage, encode the owner of the key.
@@ -1240,16 +1230,6 @@
  *
  */
 //#define MBEDTLS_PSA_CRYPTO_SPM
-
-/**
- * \def MBEDTLS_PSA_HAS_ITS_IO
- *
- * Enable the non-volatile secure storage usage.
- *
- * This is crucial on systems that do not have a HW TRNG support.
- *
- */
-//#define MBEDTLS_PSA_HAS_ITS_IO
 
 /**
  * \def MBEDTLS_RSA_NO_CRT
