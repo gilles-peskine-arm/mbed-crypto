d6d23d7bf8c99fa1ea14ae773d821b6a5da6e991
Jaeden Amero <jaeden.amero@arm.com>
2019-02-26 11:53:36 +0000

    check-names: Enable referencing Mbed TLS macros
    Add a macro whitelist which enables us to refer to Mbed TLS macros that
    are no longer present in Mbed Crypto.

--- tests/scripts/list-macros.sh
+++ tests/scripts/list-macros.sh
@@ -9,8 +9,16 @@ fi
 
 HEADERS=$( ls include/mbedtls/*.h include/psa/*.h | egrep -v 'compat-1\.3\.h' )
 
+# White-list macros we want to be able to refer to that don't exist in or
+# library, useful when referring to macros in Mbed TLS from comments.
+WHITELIST='MBEDTLS_ERR_SSL_CRYPTO_IN_PROGRESS'
+
 sed -n -e 's/.*#define \([a-zA-Z0-9_]*\).*/\1/p' $HEADERS \
     | egrep -v '^(asm|inline|EMIT|_CRT_SECURE_NO_DEPRECATE)$|^MULADDC_' \
     | sort -u > macros
+
+# Add the white-listed entries in sorted order
+echo $WHITELIST >> macros
+sort -u -o macros macros
 
 wc -l macros
