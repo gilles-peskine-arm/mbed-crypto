02a1fdc88b0afb1e57ce3f419e01c388577fd77d
Gilles Peskine <Gilles.Peskine@arm.com>
2019-07-18 00:19:48 +0200

    mbedtls_ecp_mul: remove shortcut for m = -1
    It doesn't seem to be used anywhere, so this saves a few bytes of code
    at no performance penalty.

--- library/ecp.c
+++ library/ecp.c
@@ -2495,8 +2495,9 @@ cleanup:
 }
 #endif /* ECP_SHORTWEIERSTRASS */
 
+#include <assert.h>
 /*
- * R = m * P with shortcuts for m == 1 and m == -1
+ * R = m * P with shortcut for m == 1.
  * NOT constant-time - ONLY for short Weierstrass!
  */
 static int mbedtls_ecp_mul_shortcuts( mbedtls_ecp_group *grp,
@@ -2510,12 +2511,6 @@ static int mbedtls_ecp_mul_shortcuts( mbedtls_ecp_group *grp,
     if( mbedtls_mpi_cmp_int( m, 1 ) == 0 )
     {
         MBEDTLS_MPI_CHK( mbedtls_ecp_copy( R, P ) );
-    }
-    else if( mbedtls_mpi_cmp_int( m, -1 ) == 0 )
-    {
-        MBEDTLS_MPI_CHK( mbedtls_ecp_copy( R, P ) );
-        if( mbedtls_mpi_cmp_int( &R->Y, 0 ) != 0 )
-            MBEDTLS_MPI_CHK( mbedtls_mpi_sub_mpi( &R->Y, &grp->P, &R->Y ) );
     }
     else
     {
