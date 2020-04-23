42b7d3575c8bccb7e098e7bba107960130e330e2
Gilles Peskine <Gilles.Peskine@arm.com>
2020-04-23 18:02:00 +0200

    MBEDTLS_DEPRECATED_WARNING now always emits a #warning
    MBEDTLS_DEPRECATED_WARNING now always emits a #warning since this
    branch as a whole is deprecated. So disable it from most builds.

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -702,11 +702,12 @@ component_test_full_make_gcc_o0 () {
 }
 
 component_build_deprecated () {
-    msg "build: make, full config + DEPRECATED_WARNING, gcc -O" # ~ 30s
+    msg "build: make, full config + DEPRECATED_WARNING, gcc -O, expect warnings" # ~ 30s
     scripts/config.py full
     scripts/config.py set MBEDTLS_DEPRECATED_WARNING
     # Build with -O -Wextra to catch a maximum of issues.
-    make CC=gcc CFLAGS='-O -Werror -Wall -Wextra' lib programs
+    # Expect #warning stating that Mbed Crypto is no longer updated.
+    make CC=gcc CFLAGS='-O -Werror -Wall -Wextra -Wno-error=cpp' lib programs
     make CC=gcc CFLAGS='-O -Werror -Wall -Wextra -Wno-unused-function' tests
 
     msg "test: make, full config + DEPRECATED_WARNING, expect warnings" # ~ 30s
